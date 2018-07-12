e38ae496cd9d6f1a61f7dc23649568384a2b87e3
Ivan Zabrovskiy <loriowar@gmail.com>
2018-07-12 17:51:37 +0300

    Visual improvement of Predicate edit form on the interactive view
    - unify css styles for displaying any data in table form on the interactive view
    - fix wight for select2 elements

--- web/static/js/simple_select2.js
+++ web/static/js/simple_select2.js
@@ -1,7 +1,8 @@
 export default function initializeSimpleSelect2() {
   // TODO: Display a human-readable date
   let options = {
-    allowClear: true
-  }
+    allowClear: true,
+    width: '100%'
+  };
   $('.simple-select2').select2(options);
 }
