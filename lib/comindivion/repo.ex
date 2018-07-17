233079c9e70471e1ff796bb255b7ffba1a6b6e3c
Ivan Zabrovskiy <loriowar@gmail.com>
2018-07-17 09:49:23 +0300

    Method exists as Repo extension
    - add analog of AR `exists?` method

--- lib/comindivion/repo.ex
+++ lib/comindivion/repo.ex
@@ -1,3 +1,13 @@
 defmodule Comindivion.Repo do
   use Ecto.Repo, otp_app: :comindivion
+
+  import Ecto.Query, only: [from: 2]
+
+  def exists?(queryable) do
+    query = from x in queryable, select: 1, limit: 1
+    case all(query) do
+      [1] -> true
+      [] -> false
+    end
+  end
 end
