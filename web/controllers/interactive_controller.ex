644ea8eccf1d7309e452cf75e22cc51276b749bf
Ivan Zabrovskiy <loriowar@gmail.com>
2018-07-17 10:28:28 +0300

    Message about absence of Predicates
    - on the interactive view add a big red message about absence of Predicates and link to it creation

--- web/controllers/interactive_controller.ex
+++ web/controllers/interactive_controller.ex
@@ -14,8 +14,10 @@ defmodule Comindivion.InteractiveController do
       |> select([p], {p.name, p.id})
       |> order_by([p], [asc: p.name])
       |> Repo.all
+    is_predicates_blank = predicates_for_select == []
     render conn, "index.html", mind_object_changeset: mind_object_changeset,
                                subject_object_relation_changeset: subject_object_relation_changeset,
-                               predicates_for_select: predicates_for_select
+                               predicates_for_select: predicates_for_select,
+                               is_predicates_blank: is_predicates_blank
   end
 end
