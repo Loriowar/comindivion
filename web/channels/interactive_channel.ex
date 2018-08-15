3bff154fd22f5d3e286b705b633edd288fbc1880
Ivan Zabrovskiy <loriowar@gmail.com>
2018-08-15 14:32:28 +0300

    Disable the network initialization message through the channel

--- web/channels/interactive_channel.ex
+++ web/channels/interactive_channel.ex
@@ -3,13 +3,15 @@ defmodule Comindivion.InteractiveChannel do
 
   def join("interactive:" <> user_id, _params, socket) do
     if user_id == socket.assigns.current_user.id do
-      send(self(), :after_join)
+      # Now we use REST-API for the network initialization
+      # send(self(), :after_join)
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
     end
   end
 
+  # Works together with message processing from 'web/static/js/interactive_channel.js'
   def handle_info(:after_join, socket) do
     network_data = Comindivion.Service.Interactive.NetworkInitializer.call(%{current_user: socket.assigns.current_user})
     push(socket, "interactive:network:initialize", network_data)
