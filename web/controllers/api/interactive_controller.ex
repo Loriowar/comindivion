65d75fbb3264a7361bd26ccacaefe82ec538a81e
Ivan Zabrovskiy <loriowar@gmail.com>
2018-08-15 15:26:46 +0300

    Refactor a network initialization process
    - unify query for a fetch an initial network data for API-request and for channel

--- web/controllers/api/interactive_controller.ex
+++ web/controllers/api/interactive_controller.ex
@@ -1,23 +1,9 @@
 defmodule Comindivion.Api.InteractiveController do
   use Comindivion.Web, :controller
 
-  alias Comindivion.MindObject
-  alias Comindivion.SubjectObjectRelation
-  alias Comindivion.Predicate
-  alias Comindivion.Position
-
-  import Ecto.Query, only: [from: 2]
-
   def index(conn, _params) do
-    mind_objects_query = from m in current_user_query(conn, MindObject),
-                              left_join: p in Position, on: m.id == p.mind_object_id,
-                              select: %{id: m.id, title: m.title, x: p.x, y: p.y, group: p.group}
-    mind_objects = Repo.all(mind_objects_query)
-    relations_query = from sor in current_user_query(conn, SubjectObjectRelation),
-                           join: p in ^current_user_query(conn, Predicate), on: sor.predicate_id == p.id,
-                           select: %{id: sor.id, subject_id: sor.subject_id, object_id: sor.object_id, name: p.name}
-    relations = Repo.all(relations_query)
+    network_data = Comindivion.Service.Interactive.NetworkInitializeData.call(%{current_user: current_user(conn)})
 
-    render conn, "index.json", mind_objects: mind_objects, relations: relations
+    render conn, "index.json", network_data
   end
 end
