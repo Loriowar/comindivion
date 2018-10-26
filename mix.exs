21a3b5b5fb931cd1bb2cdc688bd53cb30d80b8ba
Ivan Zabrovskiy <loriowar@gmail.com>
2018-10-26 22:00:30 +0300

    New tiny version of app (0.1.2)

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Comindivion.Mixfile do
 
   def project do
     [app: :comindivion,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
